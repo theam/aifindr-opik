996c186975d72d82b42bf859e4afa860bdd5a34e
nestor@theagilemonkeys.com <nestor@theagilemonkeys.com>
2025-02-20 14:46:28 +0000

    use golden image

--- apps/aifindr-evaluations-runner/settings.py
+++ apps/aifindr-evaluations-runner/settings.py
@@ -1,32 +1,33 @@
 import os
 from functools import lru_cache
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import field_validator
 
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
     OPIK_URL_OVERRIDE: str = "http://host.docker.internal:5173/api"
     OPENAI_API_KEY: str = ""
     ELLMENTAL_API_URL: str = ""
     ELLMENTAL_API_KEY: str = ""
-    
+
     @field_validator("*")
     def no_empty_strings(cls, v):
         if isinstance(v, str) and not v:
             raise ValueError("Field cannot be empty")
         return v
-    
-    class Config:
-        env_file = ".env"
-        extra = "ignore"  # Allow extra variables
+
 
 class EnvSettings(BaseSettings):
     settings: Settings = Settings()
 
-@lru_cache() # Cache settings to avoid re-reading the .env file on each call
+
+@lru_cache()  # Cache settings to avoid re-reading the .env file on each call
 def get_settings() -> Settings:
     if not os.getenv("SETTINGS"):
         return Settings()
     return EnvSettings().settings
 
-settings = Settings()
+
+settings = get_settings()
