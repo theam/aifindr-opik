419f3b57f63e965ad7d0b9e8fa43e60154d7607f
Álvaro <alvaro@theagilemonkeys.com>
2025-02-26 14:13:03 +0000

    Move auth env vars to the env XD

--- apps/opik-frontend/src/components/App.tsx
+++ apps/opik-frontend/src/components/App.tsx
@@ -20,11 +20,11 @@ function App() {
   useCustomScrollbarClass();
 
   const auth0Config = {
-    domain: import.meta.env.VITE_AUTH0_DOMAIN || '',
-    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID || '',
+    domain: import.meta.env.VITE_AUTH_DOMAIN || '',
+    clientId: import.meta.env.VITE_AUTH_CLIENT_ID || '',
     authorizationParams: {
       redirect_uri: window.location.origin,
-      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      audience: import.meta.env.VITE_AUTH_AUDIENCE,
     },
   };
 
